d31b48726ba811ce4d985fe85e7a9b06d71ea9e6
KayIssy54 <mwongerajoy4@gmail.com>
2026-05-14 15:32:20 +0300

    Add li tag to Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,7 +6,12 @@ const Cart = () => {
       <h2>Shopping Cart</h2>
       <ul>
         {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
-      </ul>
+      {cart.map((item, index) => (
+          <li key={index}>
+            {item.name} is in your cart.
+          </li>
+        ))}
+    </ul>
     </div>
   )
 }
